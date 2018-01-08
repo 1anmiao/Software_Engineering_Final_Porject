b2bc73151055704930c7db93f34c7ad312f480e6
Yun Lin <f0919437447@gmail.com>
2018-01-08 14:26:25 +0800

    修改controller

--- controllers/BoardController.java
+++ controllers/BoardController.java
@@ -3,13 +3,8 @@ package tetris.controllers;
 import tetris.models.Shape;
 import tetris.views.TetrisBoard;
 
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
-import sun.audio.ContinuousAudioDataStream;
-
 import javax.swing.*;
 import java.awt.*;
-import java.io.FileInputStream;
 
 /**
  * Created by saacsos on 1/12/2559.
@@ -22,29 +17,14 @@ public class BoardController {
     private boolean isFallingFinished = false;
     private boolean isStarted = false;
     private boolean isPaused = false;
+
     private int numLinesRemoved = 0;
     private int currentX = 0;
     private int currentY = 0;
     private Timer timer;
+
     private Shape currentPiece;
     private Shape.Tetrominoes[] board;
-    ContinuousAudioDataStream BGMsound = null;
-    String path1 = "C:\\Users\\user\\Downloads\\audio\\remove.wav";
-    String path2 = "C:\\Users\\user\\Downloads\\audio\\GameOver.wav";
-    String path3 = "C:\\Users\\user\\Downloads\\audio\\BGM.wav";
-    
-    //
-    AudioStream sound = null;
-    public void ImportSound(String path)
-    {
-    	try{@SuppressWarnings("resource")
-		AudioStream importsound = new AudioStream(new FileInputStream(path));
-    	sound = importsound;}catch (Exception e) {
-    		sound = null;
-            e.printStackTrace();
-        }
-    }
-    //
 
     public BoardController(int boardWidth, int boardHeight, TetrisBoard tetrisBoard) {
         this.boardWidth = boardWidth;
@@ -80,14 +60,6 @@ public class BoardController {
     }
 
     public void start() {
-    	//
-    	try{@SuppressWarnings("resource")
-		ContinuousAudioDataStream sound = new ContinuousAudioDataStream(new AudioStream(new FileInputStream(path3)).getData());
-    	BGMsound = sound;
-    	AudioPlayer.player.start(BGMsound);}catch (Exception e) {
-            e.printStackTrace();
-        }
-    	//
         if (isPaused) return;
         isStarted = true;
         isFallingFinished = false;
@@ -180,12 +152,6 @@ public class BoardController {
             timer.stop();
             isStarted = false;
             //
-            AudioPlayer.player.stop(BGMsound);
-            ImportSound(path2);
-        	if(sound != null)
-        	{
-        		AudioPlayer.player.start(sound);
-        	}
             JOptionPane.showMessageDialog(f,"you've removed "+numLinesRemoved+" lines.",
                     "GameOver.", JOptionPane.INFORMATION_MESSAGE);
             //
@@ -203,11 +169,11 @@ public class BoardController {
             if (shapeAt(x, y) != Shape.Tetrominoes.NoShape)
                 return false;
         }
+
         currentPiece = newPiece;
         currentX = newX;
         currentY = newY;
         tetrisBoard.repaint();
-        
         return true;
     }
 
@@ -249,12 +215,7 @@ public class BoardController {
         }
 
         if (numFullLines > 0) {
-        	ImportSound(path1);
-        	if(sound != null)
-        	{
-        		AudioPlayer.player.start(sound);
-        	}
-        	numLinesRemoved += numFullLines;
+            numLinesRemoved += numFullLines;
             tetrisBoard.setStatusText(String.valueOf(numLinesRemoved));
             isFallingFinished = true;
             currentPiece.setPieceShape(Shape.Tetrominoes.NoShape);
@@ -262,7 +223,7 @@ public class BoardController {
         }
     }
 
-	public void moveLeft() {
+    public void moveLeft() {
         tryMove(currentPiece, currentX - 1, currentY);
     }
     public void moveRight() {
